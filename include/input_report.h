40d32a3e8b1ab25a1eb2a4d31a7930ef3a61352a
Inoki <veyx.shaw@gmail.com>
2021-07-13 23:01:23 +0200

    Add enum for input

--- include/input_report.h
+++ include/input_report.h
@@ -1,6 +1,16 @@
 #ifndef INPUT_REPORT_H
 #define INPUT_REPORT_H
 
-
+enum InputCommand {
+    ButtonTrigged       = 0x3F,
+    
+    SIRWithSubcommand   = 0x21,
+    NFCIRFWUpdateIR     = 0x23,
+    
+    SIRFullSpeed        = 0x30,
+    NFCIRIR,           // 0x31
+    SIRUnknown1,       // 0x32
+    SIRUnknown2        // 0x33
+};
 
 #endif
