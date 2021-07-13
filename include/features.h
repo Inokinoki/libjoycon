3f7ac12accf6c16c1b524ae183d254467a82c82e
Inoki <veyx.shaw@gmail.com>
2021-07-13 23:05:02 +0200

    Add enum for features

--- include/features.h
+++ include/features.h
@@ -1,6 +1,16 @@
 #ifndef FEATURES_H
 #define FEATURES_H
 
-
+enum Features {
+    GetLastSubcmdRep    = 0x02,
+    EnableOTAFWUpgrade  = 0x70,
+    SetupMemoryRead,   // 0x71
+    MemoryRead,        // 0x72
+    MemorySectorErase, // 0x73
+    MemoryWrite,       // 0x74
+    Launch,            // 0x75
+    UnknownSend         = 0xCC,
+    UnknownRead         = 0xFE
+};
 
 #endif
