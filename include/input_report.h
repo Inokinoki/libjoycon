db2f6abdde044aac4afe41e1039ee08b8fcdd9ed
Inoki <veyx.shaw@gmail.com>
2021-07-15 23:19:08 +0200

    Add SPI read test with real device

--- include/input_report.h
+++ include/input_report.h
@@ -13,6 +13,42 @@ enum InputCommand {
     SIRUnknown2        // 0x33
 };
 
+#pragma pack(push, 1)
+struct InputReportHeader {
+    uint8_t id;
+    uint8_t timer;
+    uint8_t battery_level: 4;
+    uint8_t conn_info: 4;
+    uint8_t btn_status[3];  // TODO: Replace with a struct
+    uint8_t left_stick_status[3];   // TODO: Replace with a struct
+    uint8_t right_stick_status[3];  // TODO: Replace with a struct
+    uint8_t vibrator_input_report;
+};  // 13 bytes
+
+#include "output_report.h"
+
+struct InputReportSubcommandReply {
+    uint8_t ack;
+    uint8_t replied_subcommmand_id;
+    struct SubcommandBodySPIData spi_data;
+    uint8_t data[30];
+};
+
+struct InputReportNFCIRFWUpdate {
+    uint8_t data[37];
+};
+
+struct InputReportIMU {
+    // TODO
+    uint8_t data[36];
+};
+
+struct InputReportIMUNFCIR {
+    // TODO
+    uint8_t data[36];
+    uint8_t nfc_ir_data[313];
+};
+
 // IMU data
 #include "imu.h"
 
