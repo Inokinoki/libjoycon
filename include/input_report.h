2bf390e6401bfb8f6f9350a53be01788001316f8
Inoki <veyx.shaw@gmail.com>
2021-07-17 17:44:58 +0200

    Add SIR button status structure

--- include/input_report.h
+++ include/input_report.h
@@ -14,16 +14,48 @@ enum InputCommand {
 };
 
 #pragma pack(push, 1)
+struct SIRButtonStatus {
+    uint8_t right;
+    uint8_t share;
+    uint8_t left;
+};
+
 struct InputReportHeader {
     uint8_t id;
     uint8_t timer;
     uint8_t battery_level: 4;
     uint8_t conn_info: 4;
-    uint8_t btn_status[3];  // TODO: Replace with a struct
+    struct SIRButtonStatus btn_status;
     uint8_t left_stick_status[3];   // TODO: Replace with a struct
     uint8_t right_stick_status[3];  // TODO: Replace with a struct
     uint8_t vibrator_input_report;
 };  // 13 bytes
+
+// Right
+#define SIR_BUTTON_STATUS_MASK_Y        0x01
+#define SIR_BUTTON_STATUS_MASK_X        0x02
+#define SIR_BUTTON_STATUS_MASK_B        0x04
+#define SIR_BUTTON_STATUS_MASK_A        0x08
+#define SIR_BUTTON_STATUS_MASK_R        0x40
+#define SIR_BUTTON_STATUS_MASK_ZR       0x80
+// Left
+#define SIR_BUTTON_STATUS_MASK_DOWN     0x01
+#define SIR_BUTTON_STATUS_MASK_UP       0x02
+#define SIR_BUTTON_STATUS_MASK_RIGHT    0x04
+#define SIR_BUTTON_STATUS_MASK_LEFT     0x08
+#define SIR_BUTTON_STATUS_MASK_L        0x40
+#define SIR_BUTTON_STATUS_MASK_ZL       0x80
+// Common
+#define SIR_BUTTON_STATUS_MASK_SR       0x10
+#define SIR_BUTTON_STATUS_MASK_SL       0x20
+// Share
+#define SIR_BUTTON_STATUS_MASK_MINUS    0x01
+#define SIR_BUTTON_STATUS_MASK_PLUS     0x02
+#define SIR_BUTTON_STATUS_MASK_RSTICK   0x04
+#define SIR_BUTTON_STATUS_MASK_LSTICK   0x08
+#define SIR_BUTTON_STATUS_MASK_HOME     0x10
+#define SIR_BUTTON_STATUS_MASK_CAPTURE  0x20
+#define SIR_BUTTON_STATUS_MASK_GRIP     0x80
 
 struct InputReportSubcommandReply {
     uint8_t ack;
