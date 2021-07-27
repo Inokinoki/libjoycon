de1e0e79b887e7afa90c75ddefccbef3540225d3
Inoki <veyx.shaw@gmail.com>
2021-07-27 20:31:40 +0200

    Add declarations for PacketMCUConf

--- include/packet.h
+++ include/packet.h
@@ -10,9 +10,24 @@ struct PacketMCUReadComm
     uint8_t subcommand;
     struct MCUConfiguration conf;
     uint8_t padding;
+    // FIXME: should have 37 bytes
 };
 
 void joycon_packet_mcu_read_ack_encode(uint8_t *buffer, uint8_t timer, uint8_t index);
 void joycon_packet_mcu_read_req_encode(uint8_t *buffer, uint8_t timer, uint8_t index);
+
+struct PacketMCUConf
+{
+    struct Header header;
+    uint8_t subcommand;
+    uint8_t padding;
+    struct MCUConfiguration conf;
+    // FIXME: should have 38 bytes
+};
+
+void joycon_packet_mcu_conf_mode(uint8_t *buffer, uint8_t timer, uint8_t mode);
+void joycon_packet_mcu_conf_ir_mode(uint8_t *buffer, uint8_t timer, uint8_t mode);
+// TODO void joycon_packet_mcu_conf_registers(uint8_t *buffer, uint8_t timer, uint8_t number, uint16_t *addrs, uint8_t *vals);
+// TODO: Add a version with va_list for joycon_packet_mcu_conf_registers
 
 #endif
