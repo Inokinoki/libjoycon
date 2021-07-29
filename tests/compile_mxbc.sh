b3e9c0036e2e6195cb2bd85c1576984a4dfc19e1
Weixuan XIAO <veyx.shaw@gmail.com>
2021-07-29 22:48:03 +0200

    Remove integr tests with deps, future in examples

--- tests/compile_mxbc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcc test_rumble_mxbc.c ../src/rumble.c -g -lm \
-    `pkg-config --cflags hidapi-hidraw hidapi-libusb libudev`\
-    `pkg-config --libs hidapi-hidraw hidapi-libusb libudev`\
-    -o test_rumble_mxbc.out
