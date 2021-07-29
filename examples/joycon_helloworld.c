ba35707ff19f5c67c24e3b0f397d8e027f330f77
Weixuan XIAO <veyx.shaw@gmail.com>
2021-07-29 22:36:59 +0200

    Add more TODOs

---
+++ examples/joycon_helloworld.c
@@ -0,0 +1,15 @@
+#include <mcu.h>
+#include <rumble.h>
+#include <packet.h>
+
+#include <stdio.h>
+
+int main()
+{
+    uint8_t buffer[64];     // Buffer for an output packet
+    uint8_t timer = 0;      // Timer for the output packets, incr by 1 for each sending
+
+    // TODO: Connect and do nothing
+
+    return 0;
+}
