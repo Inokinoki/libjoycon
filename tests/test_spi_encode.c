50a6cf4ff851874e01eee21e9e5d4dcdba501fc6
Weixuan XIAO <veyx.shaw@gmail.com>
2021-07-31 10:23:06 +0200

    Add tests for SPI reading encoding

--- tests/test_spi_encode.c
+++ tests/test_spi_encode.c
@@ -1,4 +1,5 @@
 #include <spi.h>
+#include <packet.h>
 #include <assert.h>
 
 int main()
@@ -18,6 +19,35 @@ int main()
     struct SubcommandBodySPIData test_data4 = joycon_spi_read_btn_color_encode();
     assert(test_data4.offset == SPI_REGION_CONF_OFF_COLOR_BTN);
     assert(test_data4.size == 0x3);
+
+    // Test packet encoding
+    uint8_t buf[64];
+    struct Header *hdr = (struct Header *)buf;
+    struct SubcommandBody *pkt = (struct SubcommandBody *)(hdr + 1);
+
+    joycon_packet_read_spi_sn(buf, 0);
+    assert(hdr->command == Subcommand);
+    assert(pkt->subcommand == SPIFlashRead);
+    assert(pkt->spi_data.offset == joycon_spi_read_sn_encode().offset);
+    assert(pkt->spi_data.size == joycon_spi_read_sn_encode().size);
+
+    joycon_packet_read_spi_body_color(buf, 0);
+    assert(hdr->command == Subcommand);
+    assert(pkt->subcommand == SPIFlashRead);
+    assert(pkt->spi_data.offset == joycon_spi_read_body_color_encode().offset);
+    assert(pkt->spi_data.size == joycon_spi_read_body_color_encode().size);
+
+    joycon_packet_read_spi_btn_color(buf, 0);
+    assert(hdr->command == Subcommand);
+    assert(pkt->subcommand == SPIFlashRead);
+    assert(pkt->spi_data.offset == joycon_spi_read_btn_color_encode().offset);
+    assert(pkt->spi_data.size == joycon_spi_read_btn_color_encode().size);
+
+    joycon_packet_read_spi_color(buf, 0);
+    assert(hdr->command == Subcommand);
+    assert(pkt->subcommand == SPIFlashRead);
+    assert(pkt->spi_data.offset == joycon_spi_read_color_encode().offset);
+    assert(pkt->spi_data.size == joycon_spi_read_color_encode().size);
 
     return 0;
 }
