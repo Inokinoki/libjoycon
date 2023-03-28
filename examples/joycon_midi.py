e8a1dd6086d05e543b556bcc9ab93348ee4d4ca5
Inoki <veyx.shaw@gmail.com>
2023-03-28 19:34:59 +0200

    Expand note range and check when note off

--- examples/joycon_midi.py
+++ examples/joycon_midi.py
@@ -20,8 +20,9 @@ class JoyconNoteOutput:
         65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47,
         130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94,
         261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88,
-        523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77
-        # C6 is 84
+        523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77,
+        1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53,
+        # C7 is 96
     ]
 
     def __init__(self, device_id):
@@ -63,9 +64,10 @@ class JoyconNoteOutput:
         if self._buf:
             try:
                 self._timer += 1
-                libjoycon.joycon_packet_rumble_only(self._buf, self._timer & 0xF,\
-                    JoyconNoteOutput.freqs[note - JoyconNoteOutput.FREQ_OFFSET], 0)
-                self._handler.write(buf2list(self._buf, buffer_len))
+                if note - JoyconNoteOutput.FREQ_OFFSET < len(JoyconNoteOutput.freqs):
+                    libjoycon.joycon_packet_rumble_only(self._buf, self._timer & 0xF,\
+                        JoyconNoteOutput.freqs[note - JoyconNoteOutput.FREQ_OFFSET], 0)
+                    self._handler.write(buf2list(self._buf, buffer_len))
             except IOError as ex:
                 print(ex)
         self._is_busy = False
